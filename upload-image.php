227c6164dee0ac750af61f188ebb58f8a548b1eb
Udit Singh <udit@numetriclabz.com>
2016-09-27 10:59:28 +0530

    upload code

---
+++ upload-image.php
@@ -0,0 +1,26 @@
+<?php
+
+	define('upload_url', 'your upload folder url');
+
+	/** Uploads icons to server
+	  * @param String (Thumbnail URL)
+	  * @param String (Name of the icon)
+	  * @return Boolean (Result of upload)
+	  */
+	function uploadImage($thumbnail, $name){
+	
+		$thumbnail = str_replace(" ","%20",$thumbnail);
+		
+		if(file_exists(upload_url.$name)) {
+			unlink(upload_url.$name);
+		}
+		
+		//gets file from network server
+		$content = file_get_contents($thumbnail);
+		
+		//Store in the filesystem.
+		$save = file_put_contents(upload_url.$name,$content);
+	}
+	
+	uploadImage("thumbnailURL", "imagename.jpg");
+?>
